1947b111c3cbd4e09413f24291baa030296973a0
Butrint27 <70060142+Butrint27@users.noreply.github.com>
2025-01-11 15:37:09 +0100

    krijova dto per modulin ParkingSpace

---
+++ backend-dotnet7/Core/Dtos/AvailabilityMonitor/AvailabilityMonitorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_dotnet7.Core.Dtos.AvailabilityMonitor
+{
+    public class AvailabilityMonitorDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public DateTime LastCheckedTime { get; set; }
+        public TimeSpan UpTime { get; set; }
+        public TimeSpan DownTime { get; set; }
+        public TimeSpan CheckInterval { get; set; }
+        public int ParkingSpaceId { get; set; }
+    }
+}
---
+++ backend-dotnet7/Core/Dtos/ParkingSpaceManager/ParkingSpaceManagerDto.cs
@@ -0,0 +1,11 @@
+﻿namespace backend_dotnet7.Core.Dtos.ParkingSpaceManager
+{
+    public class ParkingSpaceManagerDto
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+        public decimal Pagesa { get; set; }
+        public string Kontakti { get; set; }
+        public int ParkingSpaceId { get; set; }
+    }
+}
